4e3da670593477b98d1ba0261e19d76193cbb346
鬼王 <THRAEX@aliyun.com>
2021-11-05 18:20:10 +0800

    feat(verify): mock

--- src/router/verify/index.vue
+++ src/router/verify/index.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    Two Verify
-  </div>
+  <el-dialog :visible.sync="visible">
+    <el-button type="primary" @click="handleOk">确 定</el-button>
+  </el-dialog>
 </template>
 <script>
 export default {
   data() {
-    return {}
+    return {
+      visible: true
+    }
+  },
+  methods: {
+    handleOk() {
+      console.debug(this.$store)
+      this.$store.dispatch('verify/set', 700000)
+
+      const { query, params } = this.$route
+      console.debug(query.to)
+      // this.$router.replace({ path: query.to, query: { hanzo: 'sd' }, params: { gui: 1 } })
+      this.$router.replace({ path: query.to })
+    }
   }
 }
 </script>
--- src/router/verify/router.js
+++ src/router/verify/router.js
@@ -1,87 +1,50 @@
-const VERIFY_PATH = '/verify'
+import store from '@/store'
 
-function parse(arr) {
-  if (!Array.isArray(arr) || arr.length === 0) return false
-
-  const [first, ...others] = arr
-
-  return first === undefined ? parse(others) : first
-}
+const VERIFY_PATH = '/router/verify'
+const MOCK_CODE = 700000
 
 function needVerify(route) {
-  // console.debug(route)
   const verifys = route.matched.map(({ props }) => {
     const def = props.default || {}
     const prop = typeof def === 'function' ? def(route) : def
     return prop.verify
   }).map(v => v === undefined ? v : Boolean(v)).reverse()
-  // console.debug('verifys:', verifys)
-  // const c = arr => {
-  //   if (!arr || arr.length === 0) return false
 
-  //   console.debug('arr:', arr)
-  //   const [first] = arr
-  //   return first
-  // }
-  // console.debug('undfined:', parse())
-  // console.debug('number:', parse(1))
-  // console.debug('string:', parse('1'))
-  // console.debug('empty:', parse([]))
-  /* console.group('one:')
-  console.debug('one undefined:', parse([undefined]))
-  console.debug('one false:', parse([false]))
-  console.debug('one true:', parse([true]))
-  console.groupEnd() */
-  /* console.group('two:')
-  console.debug(parse([undefined, undefined].reverse()))
-  console.debug(parse([undefined, false].reverse()))
-  console.debug(parse([undefined, true].reverse()))
-  console.debug('***********')
-  console.debug(parse([false, undefined].reverse()))
-  console.debug(parse([false, false].reverse()))
-  console.debug(parse([false, true].reverse()))
-  console.debug('***********')
-  console.debug(parse([true, undefined].reverse()))
-  console.debug(parse([true, false].reverse()))
-  console.debug(parse([true, true].reverse()))
-  console.groupEnd() */
-  console.group('three:')
-  console.debug(parse([true, undefined, undefined].reverse()))
-  console.debug(parse([true, undefined, false].reverse()))
-  console.debug(parse([true, undefined, true].reverse()))
-  console.debug('***********')
-  console.debug(parse([true, false, undefined].reverse()))
-  console.debug(parse([true, false, false].reverse()))
-  console.debug(parse([true, false, true].reverse()))
-  console.debug('***********')
-  console.debug(parse([true, true, undefined].reverse()))
-  console.debug(parse([true, true, false].reverse()))
-  console.debug(parse([true, true, true].reverse()))
-  console.debug('***********')
-  console.groupEnd()
-  // console.debug('verifys:', verifys.reverse())
-  // console.debug('verifys:', verifys)
-  // const [last] = verifys.reverse()
-  // console.debug('verifys:', last)
+  const parse = arr => {
+    if (!Array.isArray(arr) || arr.length === 0) return false
 
-  // return route.path === '/boardroom/booking/my'
-  return false
+    const [first, ...others] = arr
+
+    return first === undefined ? parse(others) : first
+  }
+
+  return parse(verifys)
+}
+
+function valid(to, next) {
+  const hasCode = MOCK_CODE === store.state.verify.code
+  return hasCode ? next : new Proxy(next, {
+    apply(target, thisArg, args) {
+      // console.log('proxyNext1')
+      // console.log(to)
+      // console.log('***************')
+      return Reflect.apply(target, thisArg, [{ path: VERIFY_PATH, query: {
+        to: to.path,
+        qeury: to.query,
+        params: to.params
+      }}])
+    }
+  })
 }
 
 function proxyNext([to, from, next]) {
-  const newNext = needVerify(to) ? new Proxy(next, {
-    apply(target, thisArg, args) {
-      console.log('proxyNext1')
-      console.log('***************')
-      return Reflect.apply(target, thisArg, [VERIFY_PATH])
-    }
-  }) : next
+  const newNext = needVerify(to) ? valid(to, next) : next
 
   return [to, from, newNext]
 }
 
 function builder(dest, proxyFn, argFn) {
-  const arg = args => argFn ? [proxyFn(argFn(args))] : proxyFn(args)
+  const arg = args => argFn ? argFn(proxyFn, args) : proxyFn(args)
   const apply = (target, thisArg, args) => Reflect.apply(target, thisArg, arg(args))
 
   return new Proxy(dest, { apply })
@@ -91,7 +54,7 @@ export default router => new Proxy(router, {
   get(target, prop) {
     const val = target[prop]
     return prop === 'beforeEach'
-      ? builder(val, dest => builder(dest, proxyNext), ([arg]) => arg)
+      ? builder(val, dest => builder(dest, proxyNext), (fn, [arg]) => [fn(arg)])
       : val
   }
 })
